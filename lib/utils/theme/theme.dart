03921488f0649f0d5d7ad2f687da91a7525e25df
dainghia99 <midvip99@gmail.com>
2024-07-20 09:53:53 +0700

    custom theme continue

---
+++ lib/utils/theme/theme.dart
@@ -0,0 +1,25 @@
+import 'package:bai_tap_lon/utils/theme/custom_theme/elevated_button_theme.dart';
+import 'package:bai_tap_lon/utils/theme/custom_theme/text_theme.dart';
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  AppTheme._();
+
+  static ThemeData lightTheme = ThemeData(
+    useMaterial3: true,
+    fontFamily: "Poppins",
+    brightness: Brightness.light,
+    primaryColor: Colors.blue,
+    scaffoldBackgroundColor: Colors.white,
+    textTheme: AppTextTheme.lightTextTheme,
+    elevatedButtonTheme: AppElevatedButtonTheme.lightElevatedButtonTheme,
+  );
+  static ThemeData dartTheme = ThemeData(
+    useMaterial3: true,
+    fontFamily: "Poppins",
+    brightness: Brightness.dark,
+    primaryColor: Colors.blue,
+    scaffoldBackgroundColor: Colors.black,
+    textTheme: AppTextTheme.dartTextTheme,
+  );
+}

7833749b208b246d2de74b7b8d6e4b442a95d67e
dainghia99 <midvip99@gmail.com>
2024-08-13 08:50:14 +0700

    Search UI succesfully

--- lib/features/shop/screens/search/search.dart
+++ lib/features/shop/screens/search/search.dart
@@ -1,3 +1,5 @@
+import 'package:bai_tap_lon/common/widgets/products/product_card_vertical.dart';
+import 'package:bai_tap_lon/containers/home_container.dart';
 import 'package:flutter/material.dart';
 
 class Search extends StatelessWidget {
@@ -5,8 +7,58 @@ class Search extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder(
-      color: Colors.green,
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          Container(
+            color: Colors.white,
+            padding: const EdgeInsets.all(0),
+            child: Stack(
+              children: [
+                TCircularContainer(
+                  width: 120,
+                  height: 120,
+                  child: Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Row(
+                      children: [
+                        Expanded(
+                          child: TextField(
+                            decoration: InputDecoration(
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                              hintText: 'Tìm kiếm',
+                            ),
+                          ),
+                        ),
+                        IconButton(
+                          icon: const Icon(Icons.search),
+                          onPressed: () {
+                            // Handle search button click
+                          },
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          GridView.builder(
+            itemCount: 4,
+            shrinkWrap: true,
+            padding: EdgeInsets.zero,
+            physics: const NeverScrollableScrollPhysics(),
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                mainAxisSpacing: 16,
+                crossAxisSpacing: 16,
+                mainAxisExtent: 288),
+            itemBuilder: (_, index) => const TProductCardVertical(),
+          ),
+        ],
+      ),
     );
   }
 }
